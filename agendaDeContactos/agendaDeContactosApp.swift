4084d41977b5ac5d7805bd1c3a6f8f762b6f997d
LuDev10 <diazldev@gmail.com>
2022-03-06 13:20:13 -0300

    Initial Commit

---
+++ agendaDeContactos/agendaDeContactosApp.swift
@@ -0,0 +1,17 @@
+//
+//  agendaDeContactosApp.swift
+//  agendaDeContactos
+//
+//  Created by LuisDiaz on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct agendaDeContactosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
